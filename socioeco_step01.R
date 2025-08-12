3df0004f731c3b9df8c9e966c55f4d7fd6aa4a92
Barros082 <arthur.barros.ar@gmail.com>
2025-08-12 15:46:43 -0300

    updating to add ibge-cnuc-socioeco join. We need rethink the goals

--- socioeco_step01.R
+++ socioeco_step01.R
@@ -84,8 +84,8 @@ wasteUC<-read_xlsx("DATA/waste_UC2022.xlsx")[-1:-6, c(-1, -4)] %>%
     COD_UC_UF=as.factor(COD_UC_UF)) %>%
   glimpse
   
-# Health - infantil mortality
-heathUC<-read_xlsx("DATA/inf_mort_UC2022.xlsx") [-1:-6,-1] %>%
+# Health - infantile mortality
+healthUC<-read_xlsx("DATA/inf_mort_UC2022.xlsx") [-1:-6,-1] %>%
   rename(COD_UC_UF=`...2`, 
          Name_UF=`...3`, 
          less1year=`...4`, 
@@ -97,4 +97,106 @@ heathUC<-read_xlsx("DATA/inf_mort_UC2022.xlsx") [-1:-6,-1] %>%
     COD_UC_UF=as.factor(COD_UC_UF)) %>%
   glimpse
 
+## Merging socioeconomic outcomes
+socioeco_join<-popUC %>% 
+  left_join(residenceUC) %>%  
+  left_join(litUC) %>%
+  left_join(waterUC) %>%  
+  left_join(sanitUC) %>%
+  left_join(wasteUC) %>% 
+  left_join(healthUC) %>%  glimpse
 
+## Merging with IBGE UC data
+socioeco_join%>%
+  left_join(PA_step01, by=c(
+    "COD_UC_UF"="CD_UC_UF"
+  )) %>% 
+  #DataExplorer::plot_missing() # just NA about CNUC data
+  # 2,429 rows
+  select(-COD_UF, -NOME_UC_UF, -CD_CNUC:-QUALI_POL, 
+         -nome_uc:-Conselho.Gestor) %>% 
+  filter(!is.na(gov_level)) %>%  #dim()#-453=1976 # CNUC data lackage
+  filter(!gov_level%in%c("so_comt", "so_plan")) %>% #dim() #-469=1507 # out treatment
+  filter(!Pop==0) %>% #dim() #-893=614
+  rowwise() %>%
+  mutate(
+    X_count = sum(c_across(resid:btw1_4years) == "X")) %>%
+  ungroup() %>%
+  filter(!X_count>0) %>% #dim() #-192=422
+  sf::st_drop_geometry() %>% 
+  select(1, Bioma, new_cat, gov_level) %>% 
+  group_by(Bioma, new_cat, gov_level) %>% 
+  summarise(n_distinct(COD_UC_UF)) %>% 
+  print(n=100)
+  
+# we need to think again if vale a pena continuar
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# understanding sensible data (i.e "X") and UC with non-PA
+
+socioeco_data<-socioeco_join%>% 
+  rowwise() %>%
+  mutate(
+    X_count = sum(c_across(resid:btw1_4years) == "X"),
+    X_type = case_when(
+      X_count == ncol(across(resid:btw1_4years)) ~ "X_all",
+      X_count == 1 ~ "X_unique",
+      X_count > 1 ~ "X_multi",
+      X_count == 0 ~ "non_X"), 
+    X_type=as.factor(X_type)) %>%
+  ungroup() %>%
+  filter(!X_count>0) %>%  # we lose 350, where 280 were all X and 70 were multi X ( the minimum was 3 columns with X and they were inf_mort and lit). 
+  #summary() 
+  select(-contains("X_")) %>% 
+  mutate(across(resid:btw1_4years, .fns=as.numeric)) %>% 
+  glimpse
+
+UC_whithout_pop<-socioeco_data %>% 
+  filter(Pop==0) %>% glimpse #1,245 (remain just 834 UC)
+
+
+#
+socioeco_data %>%
+  left_join(PA_step01, by=c(
+    "COD_UC_UF"="CD_UC_UF"
+  )) %>% 
+  DataExplorer::plot_missing()
+
+socioeco_join%>%
+  left_join(PA_step01, by=c(
+    "COD_UC_UF"="CD_UC_UF"
+  )) %>% 
+  DataExplorer::plot_missing()
+
+# garbage
+PA_ok_to_use<-PA_step01 %>% 
+  filter(!is.na(Ano.de.Criação)) %>%  
+  filter(!gov_level%in%c("so_comt", "so_plan")) %>%  
+  select(CD_UC_UF, NOME_UC_UF, Bioma, new_cat, 
+         gov_level) %>% 
+  glimpse
+
+socioeco_data %>% 
+  filter(Pop!=0) %>% 
+  left_join(PA_step01, by=c(
+    "COD_UC_UF"="CD_UC_UF"
+  )) %>%  View()
+  
