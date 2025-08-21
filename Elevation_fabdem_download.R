c97f10227c7f0983ad9f92a0c2a4c35f1f5a6b6a
Arthur Barros <barros.arthur082@gmail.com>
2025-08-21 14:33:09 -0300

    updating to cover all brazilian PA

--- Elevation_fabdem_download.R
+++ Elevation_fabdem_download.R
@@ -1,26 +1,29 @@
 library(rvest)
 library(tidyverse)
 library(sf)
+library(geobr)
 sf::sf_use_s2(F)
-
-malha_cat<-read_sf("Outputs/CAT_malha_PA_completed.gpkg") %>% glimpse()
 
 fbdemdata<-read_sf("DATA/FABDEM_data_elevation/FABDEM_v1-2_tiles.geojson") %>%
   st_transform(., "EPSG:4674") %>% 
   glimpse()
 
-polygons_cat<-st_intersection(fbdemdata, malha_cat) %>% 
+PA_shape<-read_sf("Outputs/PA_IT_shape.gpkg")%>% 
+  st_transform(., "EPSG:4674") %>% 
+  select(1)
+
+polygons_PA<-st_intersection(fbdemdata, PA_shape) %>% 
   mutate(across(.cols=c(1:3), ~as.factor(.))) %>% 
   glimpse()
 
-#polygons_cat %>% ggplot()+ geom_sf() # to see
+#polygons_PA %>% ggplot()+ geom_sf() # to see
 
 # zip that i need to download
-zipfilenames<-levels(unique(polygons_cat$zipfile_name)) %>% 
-  glimpse()
+zipfilenames<-levels(unique(polygons_PA$zipfile_name)) %>% 
+  glimpse() #17
 
-# and the tif archives that we need (122 0.0)
-levels(unique(polygons_cat$file_name)) %>% 
+# and the tif archives that we need (670 0.0)
+levels(unique(polygons_PA$file_name)) %>% 
   glimpse()
 
 code_to_filter <- zipfilenames %>% 
@@ -35,33 +38,13 @@ links <- webpage %>%
   str_subset(code_to_filter) %>%
   unique()
 
-dir.create("DATA/FABDEM_data_elevation")
+#dir.create("DATA/FABDEM_data_elevation")
 setwd("DATA/FABDEM_data_elevation")
 
 options(timeout = Inf)
 
-links_1 <- links[1]
-links_2 <- links[2]
-links_3 <- links[3]
-links_4 <- links[4]
-
-for (link in links_1) {
-  file_name <- basename(links_1)
-  download.file(links_1, file_name, mode = "wb")
+for (link in links) {
+  file_name <- basename(link)
+  download.file(link, file_name, mode = "wb")
 }
 
-for (link in links_2) {
-  file_name <- basename(links_2)
-  download.file(links_2, file_name, mode = "wb")
-}
-
-for (link in links_3) {
-  file_name <- basename(links_3)
-  download.file(links_3, file_name, mode = "wb")
-}
-
-
-for (link in links_4) {
-  file_name <- basename(links_4)
-  download.file(links_4, file_name, mode = "wb")
-}
