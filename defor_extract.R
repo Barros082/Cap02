a3fa659a3d0f080501259cab16cad245d31298f5
Arthur Barros <barros.arthur082@gmail.com>
2025-08-25 15:51:50 -0300

    updating to save

---
+++ defor_extract.R
@@ -0,0 +1,128 @@
+# Deforestation extraction
+
+library(here)
+library(tidyverse)
+library(sf)        
+library(raster)   
+library(terra)
+library(exactextractr)
+sf::sf_use_s2(F)
+
+Sys.setenv(TMPDIR = "D:/Arthur_Barros/Doutorado/CAP02/Cap02/temp")
+terraOptions(tempdir = "D:/Arthur_Barros/Doutorado/CAP02/Cap02/temp")
+
+# data 
+PA_shape<-read_sf("Outputs/PA_IT_shape.gpkg")
+
+defor22<-rast("DATA/Mapbiomas/deforestation/defor_c09_2022.tif")
+
+# cropping and masking 
+defor22 %>% crs()  #4326
+PA_crs<-PA_shape %>% st_transform(., "EPSG:4326")
+
+
+PA_crs_small <- st_filter(PA_crs, 
+                          st_as_sfc(st_bbox(defor22))) %>% 
+  st_make_valid() %>% 
+  st_cast("MULTIPOLYGON") 
+
+
+raster_list <- lapply(1:nrow(PA_crs_small), function(i) {
+  poly <- PA_crs_small[i, ]
+  code <- poly$new_code
+  message("Processando polígono ", i, " - código: ", code)
+  
+  crop_defor <- terra::crop(defor22, vect(poly))
+  mask_defor <- terra::mask(crop_defor, vect(poly))
+  r_proj <- terra::project(mask_defor, "EPSG:4674", method = "near")
+
+  return(r_proj)
+    
+})
+
+raster_list <- raster_list[!sapply(raster_list, is.null)]
+
+
+areas_list <- list()
+for (i in seq_along(raster_list)) {
+  poly <- PA_crs_small[i, ]
+  code <- poly$new_code
+  message("Extraindo áreas para polígono ", i, " - código: ", code)
+  
+  raster_proj <- terra::project(raster_list[[i]], "EPSG:5880", method = "near")
+  pixel_area <- prod(res(raster_proj))
+  poly_proj <- st_transform(poly, 5880)
+  
+  class_areas <- exact_extract(
+    raster_proj, 
+    poly_proj,  
+    fun = function(df) {  # A função recebe um dataframe 'df'
+      result <- data.frame(
+        class_1_area = sum(df$coverage_fraction[df$value == 1], na.rm = TRUE) * pixel_area,
+        class_2_area = sum(df$coverage_fraction[df$value == 2], na.rm = TRUE) * pixel_area,
+        class_3_area = sum(df$coverage_fraction[df$value == 3], na.rm = TRUE) * pixel_area,
+        class_4_area = sum(df$coverage_fraction[df$value == 4], na.rm = TRUE) * pixel_area,
+        class_5_area = sum(df$coverage_fraction[df$value == 5], na.rm = TRUE) * pixel_area,
+        class_6_area = sum(df$coverage_fraction[df$value == 6], na.rm = TRUE) * pixel_area
+      )
+      return(result)
+    },
+    summarize_df = TRUE,
+    force_df = TRUE
+  )
+  
+  class_areas$new_code <- code
+  class_areas$polygon_id <- i
+  
+  areas_list[[i]] <- class_areas
+}
+
+areas_by_class <- bind_rows(areas_list)
+
+
+
+
+
+
+
+####
+
+crop_defor<-terra::crop(defor22, vect(PA_crs_small))  
+
+mask_defor<-terra::mask(crop_defor, vect(PA_crs_small))  
+
+teste<-PA_crs_small[1:10,]
+crop_test<-terra::crop(defor22, vect(teste))
+mask_test<-terra::mask(crop_test, vect(teste))  
+
+
+
+defor22_proj <- project(mask_defor, "EPSG:4674", 
+                        method = "near")
+
+
+
+PA_crs %>%
+  split(.$new_code) %>%
+  lapply(function(df) {
+    code <- unique(df$new_code)
+    message("Processando ", code)
+    
+    raster_list <- lapply(defor22, function(p) {
+      crop_defor<-terra::crop(defor22, vect(PA_crs_small))  
+      mask_defor<-terra::mask(crop_defor, vect(PA_crs_small))
+      r <- terra::project(mask_defor, "EPSG:4674", 
+                          method = "near")
+      return(r)
+    })
+  })
+
+
+
+
+
+#extracting values
+
+
+#saving
+write_sf(, "Outputs/PA_defor.gpkg")
