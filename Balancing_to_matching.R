bfd72a486b610016b942183e9f2e1c370de9a82f
Arthur Barros <arthur.barros.ar@gmail.com>
2025-10-31 13:33:59 -0300

    waiting IT year data

--- Balancing_to_matching.R
+++ Balancing_to_matching.R
@@ -23,17 +23,27 @@ PA_full <-read_sf("Outputs/PA_IT_shape.gpkg") %>%
             by="new_code") %>%
   glimpse
 
-# cleaning
+# cleaning and add IT year
 PA_full %>%  summary()
 # climatic had 3 NA
 # defor featured 1 NA
 
-PA_full %>% 
+to_fill_TIyear<-PA_full %>% 
   filter(!is.na(prec) & !is.na(defor_amount)) %>% #dim() #1248 - right!
   select(-COD_UC:-code_it_dummy) %>% 
   mutate(
     across(.cols = c(new_cat, new_code), .fns=as.factor),
     year_ref=as.numeric(year_ref)
-  ) %>%  summary()
+  ) %>%  
+  filter(is.na(year_ref)) %>% 
+  st_drop_geometry() %>% 
+  select(1:2, year_ref) %>% 
+  glimpse()
 
-
+write.csv(to_fill_TIyear, 
+          "Outputs/temporarios/tofill_ITyears.csv")
+PA_full %>% 
+  filter(PA_name=="Buriti" & new_code=="IT_50157") %>% 
+  ggplot()+
+  geom_sf(aes(color=new_code), 
+          alpha=0.3)
