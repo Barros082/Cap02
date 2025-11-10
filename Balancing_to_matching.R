cc790fb7309f18e6a51b42ae7242b23780177c21
Arthur Barros <arthur.barros.ar@gmail.com>
2025-11-10 12:04:28 -0300

    finished!

--- Balancing_to_matching.R
+++ Balancing_to_matching.R
@@ -23,7 +23,7 @@ PA_full <-read_sf("Outputs/PA_IT_shape.gpkg") %>%
             by="new_code") %>%
   glimpse
 
-# cleaning and add IT year
+# cleaning and add IT year ----
 PA_full %>%  summary()
 # climatic had 3 NA
 # defor featured 1 NA
@@ -42,8 +42,108 @@ to_fill_TIyear<-PA_full %>%
 
 write.csv(to_fill_TIyear, 
           "Outputs/temporarios/tofill_ITyears.csv")
-PA_full %>% 
-  filter(PA_name=="Buriti" & new_code=="IT_50157") %>% 
-  ggplot()+
-  geom_sf(aes(color=new_code), 
-          alpha=0.3)
+
+# debugging some IT
+#PA_full %>% 
+#  filter(PA_name=="Toldo Chimbangue" #& new_code=="IT_7595"
+#         ) %>% 
+  #glimpse
+#  ggplot()+
+#  geom_sf(aes(color=new_code), 
+#          alpha=0.3)
+
+it_years<-read.csv("DATA/ITyears_finished.csv") %>% 
+  select(new_code, year_ref, dum) %>%  
+  rename(yr_rf=year_ref) %>% 
+  glimpse
+  
+# full data ----
+
+PA_almost_done<-PA_full %>% 
+  full_join(it_years, by="new_code") %>%
+  mutate(
+    dum=case_when(
+     is.na(dum) ~ "ok", 
+     TRUE ~ dum), 
+    year_ref=case_when(
+      new_cat=="IT" ~ NA,
+      TRUE ~ year_ref),
+    yr=coalesce(year_ref, yr_rf), 
+    yr=as.numeric(yr)) %>% 
+  filter(dum!="remove") %>% 
+  select(-year_ref, -COD_UC:-code_it_dummy, 
+         -yr_rf, -dum) %>% 
+  filter(yr<=2022) %>%
+  glimpse
+
+# Estimating PA area
+PA_finished<-PA_almost_done %>% 
+  st_transform(., "EPSG:5880") %>% #1221
+  st_make_valid() %>% 
+  mutate(
+    PA_area=st_area(geom), # m²
+    yr_creation=yr,
+    expo_time=(yr-2022)*-1, 
+    testgeom=st_is_valid(geom)
+  ) %>% 
+  filter(!is.na(defor_amount)) %>% 
+  filter(!is.na(prec)) %>%
+  #filter(testgeom==FALSE) %>% glimpse # 0 
+  select(-yr, -testgeom) %>% 
+  glimpse()  #1217
+
+#how many PA we have for each biome? ----
+biomas<-geobr::read_biomes() %>% 
+  filter(!is.na(code_biome)) %>%  
+  select(name_biome, geom) %>% 
+  st_transform(., "EPSG:5880") %>% 
+  glimpse
+
+# with repetition
+st_intersection(PA_finished, biomas) %>% 
+  st_drop_geometry() %>% 
+  filter(!new_cat%in%c(
+    "APA", "ARIE", "RPPN"
+  )) %>% 
+  group_by(name_biome, new_cat) %>% 
+  summarise(
+    N_PA=n_distinct(new_code)
+  ) %>%  
+  print(n=100)
+# Amazon, Caatinga, Cerrado and Atlantic forest is ok
+
+duplicated_by_biome<-st_intersection(PA_finished, biomas) %>% 
+  st_drop_geometry() %>% 
+  mutate(test=duplicated(new_code)) %>% 
+  filter(test==TRUE) %>% 
+  glimpse#83
+
+
+st_intersection(PA_finished, biomas) %>% 
+  st_drop_geometry() %>%
+  filter(!new_code%in%duplicated_by_biome$new_code) %>% #-166
+  filter(!new_cat%in%c(
+    "APA", "ARIE", "RPPN"
+  )) %>% 
+  group_by(name_biome, new_cat) %>% 
+  summarise(
+    N_PA=n_distinct(new_code)
+  ) %>%  
+  print(n=100)
+# Amazon, Caatinga, Cerrado and Atlantic forest is ok
+
+# preparing to match ----
+
+PA_done<-st_intersection(PA_finished, biomas) %>% 
+  #st_drop_geometry() %>%
+  filter(!new_code%in%duplicated_by_biome$new_code) %>%
+  #ggplot()+geom_sf()
+  mutate(
+    centroid=st_centroid(geom),
+    lat=st_coordinates(centroid)[,2],
+    long=st_coordinates(centroid)[,1], 
+  ) %>% 
+  glimpse
+
+saveRDS(PA_done, "Outputs/PA_balanced.rds")
+  
