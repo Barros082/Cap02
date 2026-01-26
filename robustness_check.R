1f196ac960b8366bb47c8667af190e0d2164463e
Arthur Barros <arthur.barros.ar@gmail.com>
2026-01-26 16:30:02 -0300

    updating to add sensitive analyses and spatial autocorrelation check

--- robustness_check.R
+++ robustness_check.R
@@ -1,4 +1,4 @@
-# Robutness checking
+# sensitive checking
 
 library(sf)
 library(tidyverse)
@@ -6,29 +6,79 @@ library(lmtest)
 library(sandwich)
 library(rbounds)
 library(spdep)
+library(geobr)
+library(sensemakr)
 
-# linear models and Rousebound sensitive test after matching ----
-
-#initial_data<-readRDS("Outputs/PA_balanced_with_incpcp.rds") %>% #1122
-#  filter(!new_cat%in%c("RPPN", "ARIE", "APA")) %>% 
-#  filter(!name_biome%in%c("Pampa", "Pantanal")) %>% 
-#  glimpse
-
+# OLS and sensitive test after matching ----
 matched_data<-readRDS("Outputs/Matched_data.rds")
 
-y_lm<-c("water", "sanitation", "waste", #"lit",
-     "dead_less1year", "inc_pcp_by_area", 
+# adding brazilian state code to our data
+code_states<-geobr::read_state(year = 2020) %>% 
+  select(1, 2) %>% 
+  mutate(across(.cols = c(1,2), .fns=as.factor)) %>% 
+  st_transform(., "EPSG:5880")
+
+matched_data_rmk<-list()
+for (i in seq_along(matched_data)) {
+  dfs_i<-matched_data[[i]]
+  dfs_i_names<-names(matched_data[i])
+  
+  df_i_final<-dfs_i%>%
+    st_as_sf(coords = c("long", "lat"), 
+             crs=5880) %>% 
+    st_join(
+      code_states %>% select(code_state, abbrev_state),
+      join = st_intersects, left = TRUE) %>%  
+    select(-geometry) %>%
+    left_join(dfs_i %>% 
+                select(new_code, long, lat), 
+              by="new_code") %>%
+    mutate(across(.cols=c(Treat, name_biome, subclass),
+                  .fns = as.factor)) %>% 
+    glimpse
+  matched_data_rmk[[dfs_i_names]]<-df_i_final
+}
+
+matched_data<-matched_data_rmk
+
+# solving NA issues from state codes (PA: Reserva Extrativista de Cururupu)
+#matched_data[[3]] %>% # change 1 to  
+#  filter(is.na(code_state)) %>%
+#  ggplot()+
+#  geom_sf(
+#    data = code_states %>% 
+#      filter(abbrev_state=="MA")
+#  )+
+#  geom_sf(aes(fill=PA_name))
+
+matched_data[[1]]<-matched_data[[1]] %>% 
+  mutate(abbrev_state=replace_na(abbrev_state, "MA"), 
+         code_state=replace_na(code_state, "21")) %>%  
+  #summary()
+  glimpse
+  
+matched_data[[3]]<-matched_data[[3]] %>% 
+  mutate(abbrev_state=replace_na(abbrev_state, "MA"), 
+         code_state=replace_na(code_state, "21")) %>%  
+  #summary()
+  glimpse
+
+#lapply(matched_data, summary)
+
+y_lm<-c("water", "sanitation",
+     "dead_less4year", "inc_pcp_by_area", 
      "defor_amount")
 
-form_base<- "as.factor(Treat) + as.factor(name_biome) + as.factor(subclass) + Pop + elevation_mean + lat + long + dist_to_urban + distance + PA_area + prec + expo_time + inc_pcp_by_area"
-form_inc <- "as.factor(Treat) + as.factor(name_biome) + as.factor(subclass) + Pop + elevation_mean + lat + long + dist_to_urban + distance + PA_area + prec + expo_time "
-# PS: I need remove the scale because IT just has federal. So, the model won't run. 
+form_base<- "Treat + name_biome + subclass + Pop + elevation_mean + lat + long + dist_to_urban + PA_area + prec + expo_time + inc_pcp_by_area"
+form_inc <- "Treat + name_biome + subclass + Pop + elevation_mean + lat + long + dist_to_urban + PA_area + prec + expo_time"
+# PS: I need remove the scale because all PA here was 'federal'
 
 out_glm<-list()
 out_glm_summary<-list()
 htcest_glm<-list()
-bounds_psens<-list()
 cluster_glm<-list()
+bounds_psens<-list()
+sense_models<-list()
 
 for (i in seq_along(matched_data)) {
   df<-matched_data[[i]]
@@ -52,8 +102,7 @@ for (i in seq_along(matched_data)) {
       formula = f_model,
       data = df,
       weights = df$weights,
-      family = "gaussian"
-    )
+      family = "gaussian")
     
     out_glm[[model_id]] <- model
     out_glm_summary[[model_id]] <- summary(model)
@@ -64,9 +113,9 @@ for (i in seq_along(matched_data)) {
     htcest_glm[[model_id]] <- coeftest(model,
                                        vcov = vcov_robust)
     
-    # error - subclass cluster
+    # error - states cluster
     vcov_cluster <- vcovCL(model, 
-                           cluster = ~ df$subclass)
+                           cluster = ~ df$abbrev_state)
     cluster_glm[[model_id]] <-coeftest(model, 
                                        vcov = vcov_cluster)
     
@@ -77,52 +126,106 @@ for (i in seq_along(matched_data)) {
       Gamma = 3
     )
     bounds_psens[[model_id]] <- psens_bounds
+    
+    # sensitive 
+    # names(coef(model)) # run it outside of for when sensemark feature an Error
+    sense_md<-sensemakr::sensemakr(
+      model = model,  
+      treatment= "Treat1", # I do not understand it well. 
+      benchmark_covariates = "dist_to_urban", 
+      kd = 1:3)
+    
+    sense_models[[model_id]]<- sense_md
   }
 }
+
 out_glm_summary$SUPAxSPA_water #naive
 htcest_glm$SUPAxSPA_water # robust erro
-
-#results
-cluster_glm$SUPAxSPA_water #treat effect (-)
-cluster_glm$SUPAxSPA_sanitation #treat effect (-)
-cluster_glm$SUPAxSPA_waste  #treat effect (-)
-cluster_glm$SUPAxSPA_inc_pcp_by_area
-cluster_glm$SUPAxSPA_defor_amount
-
-cluster_glm$ITxSPA_water 
-cluster_glm$ITxSPA_sanitation #treat effect (-)
-cluster_glm$ITxSPA_waste  #almost treat effect (-)
-cluster_glm$ITxSPA_inc_pcp_by_area #treat effect (-)
-cluster_glm$ITxSPA_defor_amount
-
-cluster_glm$ITxSUPA_water 
-cluster_glm$ITxSUPA_sanitation
-cluster_glm$ITxSUPA_waste  
-cluster_glm$ITxSUPA_inc_pcp_by_area #treat effect (-)
-cluster_glm$ITxSUPA_defor_amount
-
-bounds_psens # Except for children mortality, all the models has 1.
-# 0.3 was the maximum score from children mortality models.
+cluster_glm$SUPAxSPA_water # state cluster erro
+bounds_psens$SUPAxSPA_water # bound score
+sense_models$SUPAxSPA_water # sensitive
+summary(sense_models$SUPAxSPA_water)
+bounds_psens #good, Except for ITxSUPA abd SUPAxSPA_dead_less4year
 
 
-# Moran I test (spatial autocorrelation) ----
+# Spatial weight and Moran I test (spatial autocorrelation) ----
 
-spt_weights<-list()
 
+#chosen K value
+for (k in c(4, 6, 8, 10)) {
+  for (i in seq_along(matched_data)) {
+    i_df<-matched_data[[i]] %>% 
+      st_as_sf()
+    i_df_names<-names(matched_data)[i]
+    
+    centroids <- st_centroid(i_df$geom)
+    coords <- st_coordinates(centroids)
+    
+    knn_df <- knn2nb(knearneigh(coords, k = k))
+    print(summary(nb2listw(knn_df, style = "W",
+                           zero.policy = TRUE)))
+    comp <- n.comp.nb(knn_df)
+    sizes_tab <- table(comp$comp.id)
+    print(as.data.frame(sizes_tab))
+  }
+}
+#k=6 to SUPAxSPA and ITxSPA
+#k=8 to ITxSUPA
+
+# Estimating spatial weights
+spt_weights_kneighbor<-list()
 for (i in seq_along(matched_data)) {
   i_df<-matched_data[[i]] %>% 
     st_as_sf()
   i_df_names<-names(matched_data)[i]
   
-  i_df_coords<-i_df %>% 
-    mutate(point = st_centroid(geom),
-           coords = st_coordinates(point))
+  centroids <- st_centroid(i_df$geom)
+  coords <- st_coordinates(centroids)
   
-  ktest <- knn2nb(knearneigh(i_df_coords$coords))
-  k_test_dists <- unlist(nbdists(ktest, 
-                                 i_df_coords$coords))
-  print(summary(k_test_dists))
+  if(i==3){k_value<-8} 
+  else {k_value<-6}
+  
+  knn_df <- knn2nb(knearneigh(coords, k = k_value)) 
+  wght_spatial <- nb2listw(knn_df, style = "W", 
+                         zero.policy = TRUE)
+  spt_weights_kneighbor[[i_df_names]] <- wght_spatial
 }
+
+# run MoranI test
+#names(out_glm)
+#names(spt_weights_kneighbor)
+
+moranI_kneighbor <- list()
+for (w_name in names(spt_weights_kneighbor)) {
+  lw <- spt_weights_kneighbor[[w_name]]
+  
+  ols_idx <- grep(
+    paste0("^", w_name, "_"),
+    names(out_glm))
+  
+  for (i in ols_idx) {
+    model <- out_glm[[i]]
+    model_name <- names(out_glm)[i]
+  
+    res <- residuals(model)
+    moranI_kneighbor[[model_name]] <- moran.test(
+      res, lw,
+      alternative = "two.sided",
+      zero.policy = TRUE)
+  }
+}
+
+# Results
+#Just both below were spatial autocorrelated:
+#ITxSPA_inc_pcp_by_area p-value = 0.04628
+#Moran I statistic       Expectation          Variance 
+#      0.046791645      -0.002262443       0.000605892
+#ITxSPA_defor_amount p-value = 5.675e-05
+#Moran I statistic       Expectation          Variance 
+#     0.0750583148     -0.0022624434      0.0003688609
+
+
+
 
 # Saving important outputs ----
 
